c70b20e0852181316d1c80bcbe410980dd2da5f2
Hoàng Phúc <121469169+phuchh27@users.noreply.github.com>
2023-08-04 23:53:50 +0700

    fixed authguard reload (#8)
    * auth-guard reload error fixxed
    
    * add action log

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscriber, Subscription } from 'rxjs';
+import { AppState } from '../store/app.reducer';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit , OnDestroy {
   isOwnerMode = false;
   isStaffMode = false;
   isLoading = false;
-  error: string = '';
-  constructor() { }
+  error: string | null = '';
+
+ private storeSub!: Subscription;
+  private closeSub!: Subscription;
+  
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.storeSub = this.store.select('auth').subscribe(authState => {
+      this.isLoading = authState.loading;
+      this.error = authState.authError;
+    });
+  }
+
   onSwitchOWnerMode() {
     this.isOwnerMode = !this.isOwnerMode;
     this.isStaffMode = false;
@@ -21,5 +36,16 @@ export class AuthComponent {
     this.isStaffMode = !this.isStaffMode;
     this.isOwnerMode = false;
     console.log(this.isStaffMode);
+  }
+
+
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+    if (this.storeSub) {
+      this.storeSub.unsubscribe();
+    }
   }
 }
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -9,28 +9,38 @@ import {
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.reducer';
 import { Observable, map, take } from 'rxjs';
+import * as AuthActions from '../auth/store/auth.actions';
+import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private store: Store<AppState>, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private store: Store<AppState>
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
+    router: RouterStateSnapshot
   ):
     | boolean
     | UrlTree
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree> {
-    return this.store.select('auth').pipe(take(1),
-    map(authState => {
-      return authState.user;
-    }),
-    map(user => {
-      const isAuth = !!user;
-      if (isAuth) {
-        return true;
-      }
-      return this.router.createUrlTree(['/auth']);
-    }));
+    | Promise<boolean | UrlTree>
+    | Observable<boolean | UrlTree> {
+    return this.store.select('auth').pipe(
+      take(1),
+      map(authState => {
+        return authState.user;
+      }),
+      map(user => {
+        const isAuth = !!user;
+        console.log('Call AuthGuard');
+        if (isAuth) {
+          return true;
+        }
+        return this.router.createUrlTree(['/auth']);
+      })
+    );
   }
 }
