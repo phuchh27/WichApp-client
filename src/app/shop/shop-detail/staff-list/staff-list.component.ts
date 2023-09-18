627929e4f61bd59630645ed769b20c43003ea18c
phuchh27 <phuchh27@gmail.com>
2023-09-18 21:27:53 +0700

    feature: create form

--- src/app/shop/shop-detail/staff-list/staff-list.component.ts
+++ src/app/shop/shop-detail/staff-list/staff-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-staff-list',
@@ -7,10 +8,33 @@ import { Component } from '@angular/core';
 })
 export class StaffListComponent {
   createMode : boolean = false;
+
+  showPassC : boolean = false;
+  showPass : boolean = false;
+
   onCreate() {
     this.createMode = true;
   }
   onCancel(){
     this.createMode = false;
   }
+
+  onSubmit(form:NgForm){
+    if (!form.valid) {
+      console.log("form not valid");
+      return;
+    }
+    const username: string = form.value.username;
+    const email: string = form.value.email;
+    const phone: string = form.value.phone;
+    const password: string = form.value.password;
+    const repassword: string = form.value.repassword;
+
+  }
+  onShowPassC(){this.showPassC = true}
+  onHidePassC(){this.showPassC = false}
+
+  onShowPass(){this.showPass = true}
+  onHidePass(){this.showPass = false}
 }
+
