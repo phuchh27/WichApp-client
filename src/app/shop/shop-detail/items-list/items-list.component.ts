75fd3fe85691e0e44437e92927032ab4c1fbc58c
phuchh27 <phuchh27@gmail.com>
2023-09-25 20:24:29 +0700

    feat: create item component

--- src/app/shop/shop-detail/items-list/items-list.component.ts
+++ src/app/shop/shop-detail/items-list/items-list.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { Item } from 'src/app/models/item.model';
+import { apiKey } from '../../../../environments/environment.prod';
 
 @Component({
   selector: 'app-items-list',
   templateUrl: './items-list.component.html',
-  styleUrls: ['./items-list.component.css']
+  styleUrls: ['./items-list.component.css'],
 })
-export class ItemsListComponent {
+export class ItemsListComponent implements OnInit {
+  createMode: boolean = false;
+  cld: any;
 
-  createMode : boolean = false;
+  imageURL: string | null = null;
+  @ViewChild('fileInput') fileInput: any;
 
+  ngOnInit(): void {
+    this.cld = new Cloudinary({
+      cloud: { cloudName: 'dm4renyes', apiKey: apiKey },
+    });
+    console.log('Cloudinary initialized:', this.cld);
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0] as File;
+    if (file) {
+      // Đọc tệp hình ảnh và cập nhật imageURL
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageURL = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
   onCreate() {
     this.createMode = true;
@@ -19,8 +43,35 @@ export class ItemsListComponent {
     this.createMode = false;
   }
 
-  onSubmit(from: NgForm){
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      console.log('form not valid');
+      return;
+    }
+    const newItem: Item = {
+      name: form.value.itemname,
+      description: form.value.description,
+      price: form.value.price,
+      cost: form.value.cost,
+      quantity: form.value.quantity,
+      imglink: this.imageURL,
+    };
+    console.log(newItem);
 
+    // this.cld.v2.uploader.upload(
+    //   this.imageURL,
+    //   { public_id: 'item_' + newItem.name},
+    //   function (error: any, result: any) {
+    //     console.log(result);
+    //   }
+    // );
+
+    this.cld.v2.uploader.upload(
+      'https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg',
+      { public_id: 'olympic_flag' },
+      function (error: any, result: any) {
+        console.log(result);
+      }
+    );
   }
-
 }
