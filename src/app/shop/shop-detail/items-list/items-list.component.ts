f5cfe4083302a4a2d6b6af150fc0f8e43b660dc9
phuchh27 <phuchh27@gmail.com>
2023-09-25 20:25:41 +0700

    hotfit: fix code

--- src/app/shop/shop-detail/items-list/items-list.component.ts
+++ src/app/shop/shop-detail/items-list/items-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Cloudinary } from '@cloudinary/url-gen';
 import { Item } from 'src/app/models/item.model';
-import { apiKey } from '../../../../environments/environment.prod';
+
 
 @Component({
   selector: 'app-items-list',
@@ -11,16 +11,13 @@ import { apiKey } from '../../../../environments/environment.prod';
 })
 export class ItemsListComponent implements OnInit {
   createMode: boolean = false;
-  cld: any;
+ 
 
   imageURL: string | null = null;
   @ViewChild('fileInput') fileInput: any;
 
   ngOnInit(): void {
-    this.cld = new Cloudinary({
-      cloud: { cloudName: 'dm4renyes', apiKey: apiKey },
-    });
-    console.log('Cloudinary initialized:', this.cld);
+   
   }
 
   onFileSelected(event: any) {
@@ -57,21 +54,5 @@ export class ItemsListComponent implements OnInit {
       imglink: this.imageURL,
     };
     console.log(newItem);
-
-    // this.cld.v2.uploader.upload(
-    //   this.imageURL,
-    //   { public_id: 'item_' + newItem.name},
-    //   function (error: any, result: any) {
-    //     console.log(result);
-    //   }
-    // );
-
-    this.cld.v2.uploader.upload(
-      'https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg',
-      { public_id: 'olympic_flag' },
-      function (error: any, result: any) {
-        console.log(result);
-      }
-    );
   }
 }
